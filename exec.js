9281bd66fe2918c4e4c9201137a4d70025c66683
leonardo <leonardo@abcrede.com.br>
2022-05-25 17:31:52 -0300

    desisto

---
+++ exec.js
@@ -0,0 +1,18 @@
+const process = require("child_process");
+
+exports.run = async (client, message, args) => {
+  if (message.author.id !== "238755131439448074") return;
+
+  message.channel.send("Perae").then((m) => m.delete({ timeout: 500 }));
+
+  process.exec(args.joi(""), (error, stdout) => {
+    let response = error || stdout;
+    message.channel.send(
+      response,
+      { code: "asciidoc", split: "\n" }.catch((err) =>
+        message.channel.send(err)
+      )
+    );
+  });
+  return;
+};
--- index.js
+++ index.js
@@ -1,76 +1,48 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const shell = require('./run_commands.js');
+const Discord = require("discord.js");
+const config = require("./config.json");
+const teste = require("./exec.js");
 
 const client = new Discord.Client({
-    allowedMentions: {
-        parse: [`users`, `roles`],
-        repliedUser: true,
-    },
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS"
-    ],
-
+  allowedMentions: {
+    parse: [`users`, `roles`],
+    repliedUser: true,
+  },
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
 
 client.on("ready", () => {
-    console.log("Apiker está online.")
-    global.shell = new shell (client);
-})
+  console.log("Apiker está online.");
+});
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-client.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'help') {
-        message.reply(`!sec chpass-shared novasenha\n!sec chpass-vps novasenha `)
-        return;
+  if (command === "help") {
+    message.reply(`!sec chpass-shared novasenha\n!sec chpass-vps novasenha `);
+    return;
+  }
+  if (command === "chpass-shared") {
+    console.log(args[0].length);
+    if (!args.length) {
+      return message.channel.send(
+        `Você esqueceu de adicionar a nova senha, ${message.author}`
+      );
+    } else if (args[0].length < 8) {
+      return message.channel.send(
+        "A nova senha deve conter no mínimo 8 digitos."
+      );
+    } else {
+      message.channel.send(
+        `A nova senha do usuário apiki para servidores shareds é: ${args[0]}`
+      );
     }
-    if (command === 'chpass-shared') {
-        console.log(args[0].length)
-        if (!args.length) {
-            return message.channel.send(`Você esqueceu de adicionar a nova senha, ${message.author}`);
-        } else if (args[0].length < 8) {
-            return message.channel.send('A nova senha deve conter no mínimo 8 digitos.');
-        } else {
-            message.channel.send(`A nova senha do usuário apiki para servidores shareds é: ${args[0]}`);
-        }
-    }
-    
-    let command_to_run = 'uptime';
-    switch (args[1]) {
-        case 'disk_free':
-            command_to_run = 'df -h /';
-            break
-        case 'docker_status':
-            command_to_run = "docker ps -a --format  {%raw%}'{{.Names}} {{.Status}}'{%endraw%}";
-            break
-        default:
-            command_to_run = 'uptime';
-            break;
-    }
-    let data = {
-        playbook: `playbooks/command`,
-        extra_vars: {
-            "ansible_ssh_host": regData[0].ip,
-            "taskLogDir": process.env.TASK_LOG_DIR,
-            "tokenID": currentID,
-            "command_to_run": command_to_run
-        }
-    }
-    message.reply('Ok executando.. ');
-    this.playbook(data, false, ['debug'], 'ansible-playbook.log', 'command', message.id);
 
-
-
-})
+  }
+});
 
 // message.channel.send(`Comando: ${command}\n Argumento: ${args}`);
 
-
-client.login(config.token)
+client.login(config.token);
